2424496d1c5841977e3811f57eb60f0afd723a01
Łukasz Strus <61932823+lukasz-strus@users.noreply.github.com>
2026-01-18 15:57:03 +0100

    Obsługa wzorca Result w generatorze AggregateRoot
    Dodano generowanie metod Create/Update zwracających Result<T> oraz obsługę parametru Error w hookach. Wprowadzono atrybut [NoResultPattern] wyłączający ten wzorzec. Rozszerzono testy jednostkowe o oba warianty oraz automatyczne dołączanie using DomainSmith.Abstraction.Core.Result. Zaktualizowano pliki testowe i wejściowe.

--- examples/DomainSmith.AggregateRoot.Examples/Program.cs
+++ examples/DomainSmith.AggregateRoot.Examples/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Start Testu");
+﻿using DomainSmith.AggregateRoot.Examples.AggregateRoots;
+
+Console.WriteLine("Start Testu");
+
+var result = Owner.Create("Łukasz", "Strus", "lukasz.strus@gmail.com",
+    new Address("Ulica", "Warszawa", "mazowieckie", "00-000"));
+
+var owner = result.Value();
+
+Console.WriteLine($"Owner created: {owner.FirstName} {owner.LastName}, email: {owner.Email}");
