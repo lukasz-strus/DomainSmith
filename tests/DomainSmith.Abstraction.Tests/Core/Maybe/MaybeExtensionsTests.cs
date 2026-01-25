3d963f4d0b9dc1e3326db8947745db98595ed4f9
Łukasz Strus <61932823+lukasz-strus@users.noreply.github.com>
2026-01-25 14:19:24 +0100

    Dodaj testy jednostkowe dla Maybe i jego rozszerzeń
    Dodano pliki MaybeTests.cs oraz MaybeExtensionsTests.cs z testami jednostkowymi pokrywającymi podstawowe funkcjonalności klasy Maybe (None, Some, równość, wyjątki, GetHashCode) oraz jej metod rozszerzających (Bind, Match). Testy wykorzystują FluentAssertions i xUnit, zapewniając poprawność działania oraz obsługę przypadków brzegowych.

---
+++ tests/DomainSmith.Abstraction.Tests/Core/Maybe/MaybeExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿using DomainSmith.Abstraction.Core.Maybe;
+using FluentAssertions;
+
+namespace DomainSmith.Abstraction.Tests.Core.Maybe;
+
+public sealed class MaybeExtensionsTests
+{
+    [Fact]
+    public async Task Bind_WhenMaybeHasValue_ShouldInvokeFunc_AndReturnItsResult()
+    {
+        // Arrange
+        var maybe = Maybe<int>.From(2);
+        var invoked = false;
+
+        // Act
+        var result = await maybe.Bind(x =>
+        {
+            invoked = true;
+            return Task.FromResult(Maybe<string>.From((x * 10).ToString()));
+        });
+
+        // Assert
+        invoked.Should().BeTrue();
+        result.HasValue.Should().BeTrue();
+        result.Value.Should().Be("20");
+    }
+
+    [Fact]
+    public async Task Bind_WhenFuncReturnsNone_ShouldReturnNone()
+    {
+        // Arrange
+        var maybe = Maybe<int>.From(1);
+
+        // Act
+        var result = await maybe.Bind(_ => Task.FromResult(Maybe<string>.None));
+
+        // Assert
+        result.HasNoValue.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Match_WhenTaskReturnsSome_ShouldCallOnSuccess_AndReturnItsValue()
+    {
+        // Arrange
+        Task<Maybe<int>> task = Task.FromResult(Maybe<int>.From(5));
+        var successCalled = false;
+        var failureCalled = false;
+
+        // Act
+        var result = await task.Match(
+            onSuccess: x =>
+            {
+                successCalled = true;
+                return x + 1;
+            },
+            onFailure: () =>
+            {
+                failureCalled = true;
+                return -1;
+            });
+
+        // Assert
+        successCalled.Should().BeTrue();
+        failureCalled.Should().BeFalse();
+        result.Should().Be(6);
+    }
+}
---
+++ tests/DomainSmith.Abstraction.Tests/Core/Maybe/MaybeTests.cs
@@ -0,0 +1,143 @@
+﻿using DomainSmith.Abstraction.Core.Maybe;
+using FluentAssertions;
+
+namespace DomainSmith.Abstraction.Tests.Core.Maybe;
+
+public sealed class MaybeTests
+{
+    [Fact]
+    public void None_ShouldHaveNoValue()
+    {
+        // Act
+        var maybe = Maybe<string>.None;
+
+        // Assert
+        maybe.HasNoValue.Should().BeTrue();
+        maybe.HasValue.Should().BeFalse();
+    }
+
+    [Fact]
+    public void From_WithNonNullReference_ShouldHaveValue_AndExposeValue()
+    {
+        // Act
+        var maybe = Maybe<string>.From("abc");
+
+        // Assert
+        maybe.HasValue.Should().BeTrue();
+        maybe.HasNoValue.Should().BeFalse();
+        maybe.Value.Should().Be("abc");
+    }
+
+    [Fact]
+    public void From_WithNullReference_ShouldHaveNoValue()
+    {
+        // Act
+        var maybe = Maybe<string>.From(null!);
+
+        // Assert
+        maybe.HasNoValue.Should().BeTrue();
+        maybe.HasValue.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ImplicitConversion_FromValue_ShouldCreateMaybe_EquivalentToFrom()
+    {
+        // Arrange
+        const string value = "xyz";
+
+        // Act
+        Maybe<string> fromImplicit = value;
+        var fromFactory = Maybe<string>.From(value);
+
+        // Assert
+        fromImplicit.Equals(fromFactory).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Value_WhenNone_ShouldThrowInvalidOperationException()
+    {
+        // Arrange
+        var maybe = Maybe<string>.None;
+
+        // Act
+        var act = () => _ = maybe.Value;
+
+        // Assert
+        act.Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("The value can not be accessed because it does not exist.");
+    }
+
+    [Fact]
+    public void Equals_WhenBothNone_ShouldBeTrue()
+    {
+        // Arrange
+        var a = Maybe<string>.None;
+        var b = Maybe<string>.None;
+
+        // Act / Assert
+        a.Equals(b).Should().BeTrue();
+        a.Equals((object)b).Should().BeTrue();
+        a.GetHashCode().Should().Be(0);
+        b.GetHashCode().Should().Be(0);
+    }
+
+    [Fact]
+    public void Equals_WhenOneNone_AndOtherHasValue_ShouldBeFalse()
+    {
+        // Arrange
+        var none = Maybe<string>.None;
+        var some = Maybe<string>.From("a");
+
+        // Act / Assert
+        none.Equals(some).Should().BeFalse();
+        some.Equals(none).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_WhenBothHaveSameValue_ShouldBeTrue()
+    {
+        // Arrange
+        var a = Maybe<int>.From(123);
+        var b = Maybe<int>.From(123);
+
+        // Act / Assert
+        a.Equals(b).Should().BeTrue();
+        a.Equals((object)b).Should().BeTrue();
+        a.GetHashCode().Should().Be(b.GetHashCode());
+    }
+
+    [Fact]
+    public void Equals_ObjectOverload_WhenObjectIsUnderlyingValue_ShouldBeTrue()
+    {
+        // Arrange
+        var maybe = Maybe<int>.From(10);
+
+        // Act / Assert
+        // ReSharper disable once SuspiciousTypeConversion.Global
+        maybe.Equals((object)10).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Equals_ObjectOverload_WhenNull_ShouldBeFalse()
+    {
+        // Arrange
+        var maybe = Maybe<int>.From(10);
+
+        // Act / Assert
+        maybe.Equals(((object?)null)!).Should().BeFalse();
+    }
+
+    [Fact]
+    public void GetHashCode_WhenHasValue_ShouldMatchUnderlyingValueHashCode()
+    {
+        // Arrange
+        var maybe = Maybe<string>.From("hash");
+
+        // Act
+        var hash = maybe.GetHashCode();
+
+        // Assert
+        hash.Should().Be("hash".GetHashCode());
+    }
+}
